a0d7c9d9725ef0a5af9b5f68adc4de7c61117900
Sudhansu Jena <80307425+Sudhansujena486@users.noreply.github.com>
2023-02-17 23:15:50 +0530

    Add files via upload

---
+++ string_reversal_without_using_loop_and_buildin_function.c
@@ -0,0 +1,16 @@
+// Printing string in reverse order without indexing,looping or using any builtin function
+#include <stdio.h>
+ void reverse(char* s){
+      if(*s!='\0'){
+      reverse(s+1);
+       printf("%c",*s);
+      }
+}
+int main() {
+    char str[5]="hello";
+    
+   printf("original string> %s\n",str);
+   printf("reversed string>> ");
+   reverse(str);
+    return 0;
+}
